a30f99a7d5c6313ae317903b3d1d4c98a5bb58e2
MyKhant <myokhantkyaw148@gmail.com>
2023-01-09 12:02:00 +0630

    endpoint restirction project

---
+++ src/main/java/com/example/endpointrestirction/EndpointRestirctionApplication.java
@@ -0,0 +1,13 @@
+package com.example.endpointrestirction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndpointRestirctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndpointRestirctionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/endpointrestirction/EndpointRestirctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.endpointrestirction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndpointRestirctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
